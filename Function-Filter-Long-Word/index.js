aa667d2648c984c4c9816953012e59f1a4b1ad75
Sinuhe Montero <52340856+Sinuhem23@users.noreply.github.com>
2021-08-06 17:07:09 -0400

    Add files via upload

--- Function-Filter-Long-Word/index.js
+++ Function-Filter-Long-Word/index.js
@@ -1,1 +1,12 @@
-
+// Write a function filterLongWords that takes an array of words and a number i and returns the array of words that are longer than i characters long.
+// //////////////////////////////////////////////////////////////////
+filterLongWords = (arr, num) => {
+  newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > num) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
+console.log(filterLongWords(['hello', 'there', 'worlds'], 5));
