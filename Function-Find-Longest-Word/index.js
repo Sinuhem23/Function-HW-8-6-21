99ce240b4f1181984ac849c48ed30bbe7ffe5bc9
Sinuhe Montero <52340856+Sinuhem23@users.noreply.github.com>
2021-08-06 17:06:35 -0400

    Add files via upload

--- Function-Find-Longest-Word/index.js
+++ Function-Find-Longest-Word/index.js
@@ -1,1 +1,17 @@
+// Write a function findLongestWord that takes an array of words and returns the length of the longest one.
+///////////////////////////////////////////////////////////////////
+var prompt = require('prompt-sync')();
+var user_Array = prompt('Enter something: ');
 
+function findLongestWord(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+      console.log('Longest word is: ' + words[i] + '.');
+    }
+  }
+  return 'The length is: ' + maxLength;
+}
+console.log(findLongestWord(user_Array));
