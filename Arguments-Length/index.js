c2aeed9b0454f2de79c091bd93424cd82bc38635
Sinuhe Montero <52340856+Sinuhem23@users.noreply.github.com>
2021-08-06 17:05:06 -0400

    Add files via upload

--- Arguments-Length/index.js
+++ Arguments-Length/index.js
@@ -1,1 +1,10 @@
+// Write a function that returns the number of arguments passed to the function when called.
+////////////////////////////////////////////////////////////////////
+function sumAll() {
+  for (let i = 0; i < arguments.length; i++) {
+    return arguments.length;
+  }
+}
+x = sumAll(1, 12, 4, 8, 3);
 
+console.log(x);
